153cb4c101249f329e449172b7d9678973c4e0cb
iZeroKim <kimgichuru6@gmail.com>
2022-03-17 18:24:17 +0300

    Implement user authentication

--- ProjectMan/app/src/main/java/com/wasusi/projectman/MainActivity.kt
+++ ProjectMan/app/src/main/java/com/wasusi/projectman/MainActivity.kt
@@ -1,11 +1,42 @@
 package com.wasusi.projectman
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import com.wasusi.projectman.auth.LoginFragment
+import com.wasusi.projectman.view.DashboardFragment
+import com.wasusi.projectman.viewModel.SharedViewModel
+
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var sharedViewModel: SharedViewModel
+    private var userId: Int? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.container, LoginFragment.newInstance())
+            .commitNow()
+        sharedViewModel = ViewModelProvider(this).get(SharedViewModel::class.java)
+
+//            if(::sharedViewModel.getUserId().isInitialized){
+//
+//                supportFragmentManager.beginTransaction()
+//                    .replace(R.id.container, DashboardFragment.newInstance())
+//                    .commitNow()
+//            } else{
+//                supportFragmentManager.beginTransaction()
+//                    .replace(R.id.container, LoginFragment.newInstance())
+//                    .commitNow()
+//            }
+
+
     }
+
+
+
+
 }
---
+++ ProjectMan/app/src/main/java/com/wasusi/projectman/network/requests/LoginRequest.kt
@@ -0,0 +1,10 @@
+package com.wasusi.projectman.network.requests
+
+import com.google.gson.annotations.SerializedName
+
+data class LoginRequest(
+    @SerializedName("email")
+    var email:String,
+    @SerializedName("password")
+    var password:String
+)
